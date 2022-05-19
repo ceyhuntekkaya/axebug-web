6b3d7afb784b069dda46a57f25a905b8730f80af
ceyhuntekkaya <ceyhuntekkaya@gmail.com>
2022-05-19 14:48:23 +0300

    control

--- src/app/StudentPanel.js
+++ src/app/StudentPanel.js
@@ -62,11 +62,11 @@ export default function StudentPanel() {
           <Square col="4" backgroundColor="white" to="/chapter"><b>AXE 4 SKILLS</b></Square>
 
           <Square col="4" backgroundColor="white" to="/speling"><b>SPELLING</b> </Square>
-          <div className="col-4 p-2"><a href=""><img className='w-100' src={`assets/l7.png`} /></a></div>
+          <div className="col-4 p-2"><img className='w-100' src={`assets/l7.png`} /></div>
           <Square col="4" backgroundColor="white" to="/wordbank"><b>WORDBANK</b></Square>
 
           <Square col="4" backgroundColor="white" to="/exam"><b>GOALS AND ACHIEVEMENTS</b> </Square>
-          <div className="col-4 p-2"><a href=""><img className='w-100' src={`assets/l1.png`} /></a></div>
+          <div className="col-4 p-2"><img className='w-100' src={`assets/l1.png`} /></div>
           <Square col="4" backgroundColor="white" to="/quiz"><b>REPORT</b></Square>
         </div>
       </React.Fragment>
