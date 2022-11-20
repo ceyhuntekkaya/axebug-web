e2dd7da54f83d6eda84d140abf7a6e72f50b65cf
ceyhuntekkaya <ceyhuntekkaya@gmail.com>
2022-11-20 16:48:15 +0300

    aws

--- src/app/DijitalContents.js
+++ src/app/DijitalContents.js
@@ -50,7 +50,7 @@ export default function DijitalContents() {
                     <div className="col-3 p-2"><a target="_blank" href="HTML/Chapter4/index.html"><img className='w-100' src={`assets/ch4.png`} /></a></div>
 
                     <div className="col-3 p-2"><a target="_blank" href="HTML/Activite1/index.html"><img className='w-100' src={`assets/ab1.png`} /></a></div>
-                    <div className="col-3 p-2"><a target="_blank" href="HTML/Activite2/index.html"><img className='w-100' src={`assets/ab2.png`} /></a></div>
+                    <div className="col-3 p-2"><a target="_blank" href="HTML/Activite2v3/index.html"><img className='w-100' src={`assets/ab2.png`} /></a></div>
                     <div className="col-3 p-2"><a target="_blank" href="HTML/Activite3/index.html"><img className='w-100' src={`assets/ab3.png`} /></a></div>
                     <div className="col-3 p-2"><a target="_blank" href="HTML/Activite4/index.html"><img className='w-100' src={`assets/ab4.png`} /></a></div>
 
