07333e1ab0f0aabc801b13e25f63caaaeb595e3c
ceyhuntekkaya <ceyhun.tekkaya@gmail.com>
2023-02-03 17:10:16 +0300

    control

--- public/HTML/Show_Time/mobile/javascript/bookmark_config.js
+++
@@ -1,1 +0,0 @@
-﻿var ols=[{caption:"Chapter 1 - Space Day",page:"2",url:"",level:"1",children:[]},{caption:"Chapter 2 - Robot Day",page:"3",url:"",level:"1",children:[]},{caption:"Chapter 3 - Music Day",page:"4",url:"",level:"1",children:[]},{caption:"Chapter 4 - Gansgter Day",page:"1",url:"",level:"1",children:[]}];
